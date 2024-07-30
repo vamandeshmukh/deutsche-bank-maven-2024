b908fbdccb6d18408f6e07dc4c9738e83a9dacbd
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-07-30 10:47:29 +0530

    day-2

--- src/main/java/com/db/demo/streams/StreamsDemo.java
+++ src/main/java/com/db/demo/streams/StreamsDemo.java
@@ -2,6 +2,7 @@ package com.db.demo.streams;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class StreamsDemo {
 
@@ -15,6 +16,51 @@ public class StreamsDemo {
 		
 		// print names starting with 'S'
 
+		System.out.println("Using for loop:");
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).startsWith("S")) {
+				System.out.println(list.get(i));
+			}
+		}
+
+		System.out.println("Using for each loop:");
+		for (String name: list) {
+			if (name.startsWith("S")) {
+				System.out.println(name);
+			}
+		}
+
+		System.out.println("Using foreach method:");
+		list.forEach((name) -> {
+			if (name.startsWith("S")) {
+				System.out.println(name);
+			}
+		});
+		
+//		filter, map, reduce, skip, limit etc 
+		
+//		Stream<T>
+		System.out.println("Using Stream API:");
+
+		list.stream().filter(name -> name.startsWith("S")).toList().forEach(name -> System.out.println(name));
+
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
