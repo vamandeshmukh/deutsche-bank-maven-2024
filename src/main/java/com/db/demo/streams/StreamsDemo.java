6b472db72b08b03594aee746915b952e37bf10a1
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-07-30 10:29:00 +0530

    day-2

---
+++ src/main/java/com/db/demo/streams/StreamsDemo.java
@@ -0,0 +1,18 @@
+package com.db.demo.streams;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class StreamsDemo {
+
+	public static void main(String[] args) {
+
+		ArrayList<String> list = new ArrayList<>();
+
+		list.addAll(Arrays.asList("Sonu", "Monu", "Tonu", "Soni", "Ponu"));
+
+		System.out.println(list);
+
+	}
+
+}
