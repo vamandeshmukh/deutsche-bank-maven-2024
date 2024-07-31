e0acdb152b9e7e49c9e07c3a39d0c66abc953a0f
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-07-31 15:02:16 +0530

    day-3

--- src/test/java/com/db/demo/math/CalcTests.java
+++ src/test/java/com/db/demo/math/CalcTests.java
@@ -1,6 +1,7 @@
 package com.db.demo.math;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -17,9 +18,9 @@ public class CalcTests {
 	@Test
 	public void testAddNums2() {
 		Calc calc = new Calc();
-		Integer expected = 11;
-		Integer actual = calc.addNums(6, 5);
-		assertEquals(expected, actual);		
+		Integer unexpected = 11;
+		Integer actual = calc.addNums(5, 5);
+		assertNotEquals(unexpected, actual);		
 	}
 }
 
