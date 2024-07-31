52217df383672f727a0882570233aa67378e0ad1
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-07-31 16:35:33 +0530

    day-3

--- src/test/java/com/db/demo/math/CalcTests.java
+++ src/test/java/com/db/demo/math/CalcTests.java
@@ -13,57 +13,55 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class CalcTests {
-	
+
 	private static Calc calc;
 
 	@BeforeAll
 	static void setUp() {
-		// code runs once before all the test cases 
+		// code runs once before all the test cases
 		calc = new Calc();
 	}
 
 	@AfterAll
 	static void tearDown() {
-		// code runs once after all the test cases 
+		// code runs once after all the test cases
 		calc = null;
 	}
-	
+
 	@BeforeEach
 	void m1() {
-		
+
 	}
-	
+
 	@AfterEach
 	void m2() {
-		
+
 	}
 
 	@Test
 	public void testAddNums() {
-		assertEquals(10, calc.addNums(5, 5));		
+		assertEquals(10, calc.addNums(5, 5));
 	}
 
 	@ParameterizedTest
-	@ValueSource(ints = {10, 20, 20, 40, 50})
+	@ValueSource(ints = { 10, 20, 20, 40, 50 })
 	public void testAddNums3(int num) {
-		int expected = num + num; 
-		assertEquals(expected, calc.addNums(num, num));		
+		int expected = num + num;
+		assertEquals(expected, calc.addNums(num, num));
 	}
 
-	
 	@Disabled
 	@Test
 	public void testAddNums2() {
-		assertNotEquals(11, calc.addNums(5, 5));		
+		assertNotEquals(11, calc.addNums(5, 5));
 	}
-	
+
 	@Test
 	public void testSubNums() {
-		assertEquals(5, calc.subNums(10, 5));		
+		assertEquals(5, calc.subNums(10, 5));
 	}
-	
-}
 
+}
 
 //package com.db.demo.math;
 //
@@ -94,13 +92,3 @@ public class CalcTests {
 //	}
 //	
 //}
-
-
-
-
-
-
-
-
-
-
