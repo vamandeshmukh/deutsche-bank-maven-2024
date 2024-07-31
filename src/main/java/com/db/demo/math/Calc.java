50eb7a946ad0171ce26c6b8133174b567a5c5978
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-07-31 15:23:56 +0530

    day-3

--- src/main/java/com/db/demo/math/Calc.java
+++ src/main/java/com/db/demo/math/Calc.java
@@ -1,9 +1,22 @@
 package com.db.demo.math;
 
 public class Calc {
-	
+
 	public int addNums(Integer i, Integer j) {
 		return i + j;
 	}
 
+	public Integer subNums(Integer i, Integer j) {
+		return i - j;
+	}
+
 }
+
+// BDD - 
+// Behaviour Driven Development 
+// write business code first and then test it 
+
+// TDD - 
+// Test Driven Development 
+// Write test cases first and then write business code 
+// test-first approach 
--- src/test/java/com/db/demo/math/CalcTests.java
+++ src/test/java/com/db/demo/math/CalcTests.java
@@ -3,31 +3,73 @@ package com.db.demo.math;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class CalcTests {
+	
+	private static Calc calc;
+
+	@BeforeAll
+	static void setUp() {
+		// code runs once before all test cases 
+		calc = new Calc();
+	}
+
+	@AfterAll
+	static void tearDown() {
+		// code runs once after all test cases 
+		calc = null;
+	}
 
 	@Test
 	public void testAddNums() {
-		Calc calc = new Calc();
-		Integer expected = 10;
-		Integer actual = calc.addNums(5, 5);
-		assertEquals(expected, actual);		
+		assertEquals(10, calc.addNums(5, 5));		
 	}
 
 	@Test
 	public void testAddNums2() {
-		Calc calc = new Calc();
-		Integer unexpected = 11;
-		Integer actual = calc.addNums(5, 5);
-		assertNotEquals(unexpected, actual);		
+		assertNotEquals(11, calc.addNums(5, 5));		
 	}
+	
+	@Test
+	public void testSubNums() {
+		assertEquals(5, calc.subNums(10, 5));		
+	}
+	
 }
 
 
-
-
-
+//package com.db.demo.math;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertNotEquals;
+////import org.junit.jupiter.api.Assertions;
+//
+//import org.junit.jupiter.api.Test;
+//
+//public class CalcTests {
+//
+//	@Test
+//	public void testAddNums() {
+//		Calc calc = new Calc();
+//		Integer expected = 10;
+//		Integer actual = calc.addNums(5, 5);
+//		assertEquals(expected, actual);		
+////		Assertions.assertEquals(expected, actual);		
+//	}
+//
+//	@Test
+//	public void testAddNums2() {
+//		Calc calc = new Calc();
+//		Integer unexpected = 11;
+//		Integer actual = calc.addNums(5, 5);
+//		assertNotEquals(unexpected, actual);		
+////		Assertions.assertNotEquals(unexpected, actual);		
+//	}
+//	
+//}
 
 
 
